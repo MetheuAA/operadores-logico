adeef9a982a7946388ee01e98d7c5bfdeaf061d1
MetheuAA <matheo200817@gmail.com>
2024-03-27 11:21:51 -0400

    finalizada questao6

---
+++ questao6.js
@@ -0,0 +1,8 @@
+/* Como você pode verificar se um número está dentro de um intervalo de 
+10 a 20 em JavaScript usando operadores lógicos? 
+
+function estaNoIntervalo(numero) {
+    return numero >= 10 && numero <= 20;
+}
+
+*/
